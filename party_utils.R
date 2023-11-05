8161b8de1f63eed0c5ba5fdbad5e7978f9c3a1c5
favstats <fabio.votta@gmail.com>
2023-11-05 22:39:56 +0100

    refactoring

--- party_utils.R
+++ party_utils.R
@@ -1,44 +1,84 @@
-color_dat <- tibble(
-  colors = c("#e6194B", "#f5911d", "#00008b", "#00FF00", "#524fa1", "#ff1a1a", "#f1c851", "#f04949", "#6495ed", "#2598d4", "#89cff0", "#89cff0", "#9A6324", "#fffac8", "#800000", "#00008b"),
-  party = c("Drugo", "DCG", "DF", "URA", "DPS", "SDP", "ES!", "SD", "SNP", "UCG", "Vlada Crne Gore", "Demos", "HGI", "LP", "Prava Crna Gora
-", "ASh"))
-
-party_dict <- tibble(coalition = c("Count Bravely! (Democrats-URA)", "Count Bravely! (Democrats-URA)", "ZBCG (NSD–DNP-RP)", "PES!", "Together! (DPS-SD-LP-UDSh)", "Together! (DPS-SD-LP-UDSh)", "Together! (DPS-SD-LP-UDSh)", "SDP", "For You! (SNP-Demos)", "For You! (SNP-Demos)", "UCG", "HGI", "People's coalition (Prava-DHP)", "Albanian Forum Coalition"),
-                     party = c("URA", "DCG", "DF", "ES!", "DPS", "LP", "SD", "SDP", "SNP", "Demos", "UCG", "HGI", "Prava Crna Gora", "ASh"))
-
-# 638633769597292
-# Albanian Forum Coalition
-
-color_dat <- party_dict %>% 
-  distinct(coalition) %>% 
-  mutate(colors = case_when(
-    str_detect(coalition, "URA") ~ "#69bd44",
-    str_detect(coalition, "ZBCG") ~ "#0037a0",
-    str_detect(coalition, "PES") ~ "#f1c851",
-    str_detect(coalition, "Together") ~ "#524fa1",
-    str_detect(coalition, "SDP") ~ "#ff1a1a",
-    str_detect(coalition, "For You") ~ "#00a2e4",
-    str_detect(coalition, "UCG") ~ "#6585e7",
-    str_detect(coalition, "HGI") ~ "#f08080",
-    str_detect(coalition, "DHP") ~ "#0172bb",
-    str_detect(coalition, "Albanian Forum Coalition") ~ "#00008b",
-  ))
-
-
-# # A tibble: 10 × 1
-# coalition                     
-# <chr>                         
-#   1 Count Bravely! (Democrats-URA)
-# 2 ZBCG (NSD–DNP-RP)             
-#   4 Together! (DPS-SD-LP-UDSh)    
-# 5 SDP                           
-# 7 UCG                           
 # 
-# # A tibble: 10 × 10
-#    advertiser_name                potent…¹ spend…² spend…³ spend…⁴ impre…⁵ impre…⁶ impre…⁷ n_ids colors
-#    <chr>                             <dbl>   <dbl>   <dbl>   <dbl>   <dbl>   <dbl>   <dbl> <int> <chr> 
-#  2 Count Bravely! (Democrats-URA) 21685463   15772   51536  33654   1.55e7  1.84e7  1.70e7   364 #69bd…
-# 7 SDP                             8790097     594   10103   5348.  2.18e6  2.61e6  2.40e6    97 #ff1a…
-# 8 Together! (DPS-SD-LP-UDSh)      5056057     552    6143   3348.  1.71e6  2.07e6  1.89e6    57 #524f…
-# 9 ZBCG (NSD–DNP-RP)               6000061     259    6239   3249   1.28e6  1.55e6  1.42e6    61 #0037…
-# 10 NA                                   NA
+# print("hello")
+# 
+# print(getwd())
+
+all_dat <- readRDS("../data/all_dat.rds")
+
+# print("hello")
+
+sets <- jsonlite::fromJSON("../settings.json")
+
+
+
+
+options(scipen = 999)
+
+
+# wtm_data %>% count(party,sort = T)
+
+
+# source("../party_utils.R")
+color_dat <- tibble(
+  colors = c("#00b13d", "#80c31c", "#0a2cca", "#008067", "#bf0000", "#ff0000", "#6f2421", "#02a6e9", "#92107d", "#04d3d4", "#242b57", "#66cdaa", "#242b57", "#006b28", "#012758", "#ea5b0b", "#582c83", "#698c0c", "#fdfd00", "#8da6d6", "#dc1f26"),
+  party = c("D66", "GroenLinks", "VVD", "CDA", "SP", "PvdA", "FvD", "ChristenUnie", "50PLUS", "Alliantie", "BVNL", "DENK", "Ja21", "PvdD", "PVV", "SGP", "Volt Nederland", "BBB", "BIJ1", "NSC", "GroenLinks-PvdA"))
+
+
+most_left_party <- "VVD"
+
+
+scale_fill_parties <- function(...){
+  ggplot2:::manual_scale(
+    'fill',
+    values = setNames(color_dat$colors, color_dat$party),
+    ...
+  )
+}
+scale_color_parties <- function(...){
+  ggplot2:::manual_scale(
+    'color',
+    values = setNames(color_dat$colors, color_dat$party),
+    ...
+  )
+}
+
+
+election_dat30 <- readRDS("../data/election_dat30.rds") %>%
+  # left_join(all_dat) %>%
+  rename(internal_id = page_id) %>%
+  filter(party != "And")  %>%
+  filter(is.na(no_data)) %>%
+  mutate(party = ifelse(party %in% c("GroenLinks", "PvdA"), "GroenLinks-PvdA", party))
+
+
+election_dat7 <- readRDS("../data/election_dat7.rds") %>%
+  # left_join(all_dat) %>%
+  rename(internal_id = page_id) %>%
+  filter(party != "And")  %>%
+  filter(is.na(no_data)) %>%
+  mutate(party = ifelse(party %in% c("GroenLinks", "PvdA"), "GroenLinks-PvdA", party))
+
+# saveRDS(election_dat30, "../data/election_dat30.rds")
+# saveRDS(election_dat7, "../data/election_dat7.rds")
+
+fin <- (as.Date(election_dat30$ds[1])-lubridate::days(1))
+begin7 <- fin-lubridate::days(6)
+begin30 <- fin-lubridate::days(29)
+
+
+last7days_string <- paste0(create_date(begin7), " - ", paste(create_date(fin), lubridate::year(fin)))
+last30days_string <- paste0(create_date(begin30), " - ", paste(create_date(fin), lubridate::year(fin)))
+
+
+
+the_currency <- election_dat30 %>%
+  count(main_currency, sort = T) %>%
+  slice(1) %>%
+  pull(main_currency)
+
+if(the_currency == "EUR"){
+  currency_symbol <- "€"
+} else {
+  currency_symbol <- "CURRENCY"
+}
+
